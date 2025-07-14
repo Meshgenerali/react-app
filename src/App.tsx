097d57c1652eb65ae900ed242c0274911a2b989c
meshcodes <meshcodes@kali.meshcodes.com>
2025-07-15 01:58:45 +0300

    props and event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["Nairobi", "Mombasa", "Eldoret", "Kisumu", "Nakuru"];
+
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectedItem={handleSelectedItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectedItem: (item: string) => void;
 }
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectedItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -18,7 +19,10 @@ function ListGroup({ items, heading }: Props) {
                 : "list-group-item"
             }
             key={item}
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectedItem(item);
+            }}
           >
             {item}
           </li>
