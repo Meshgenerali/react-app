a99ab4dde0d4f9ed0db92d69f07d04d20de77d2d
meshcodes <meshcodes@kali.meshcodes.com>
2025-07-14 18:56:37 +0300

    Loops and conditional statements

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["Nairobi", "Mombasa", "Eldoret", "Kisumu", "Nakuru"];
-  let itemArray = [];
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
-      {itemArray.length === 0 && <p>Please add some items to the list.</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
