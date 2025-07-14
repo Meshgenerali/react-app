76d50748574d471e0e1d579396f0afb986f66de2
meshcodes <meshcodes@kali.meshcodes.com>
2025-07-14 17:06:04 +0300

    Loops and conditional statements

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
 function ListGroup() {
+  let items = ["Nairobi", "Mombasa", "Eldoret", "Kisumu", "Nakuru"];
+  let itemArray = [];
+
   return (
     <>
       <h1>List</h1>
+      {itemArray.length === 0 && <p>Please add some items to the list.</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
